587cd3666199be8b722d7792bd71f8e863727226
Sergey <jofreyswb@gmail.com>
2017-02-15 21:12:28 +0200

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,13 +10,26 @@
         <!-- parallax layers -->
         <div class="parallax-layer" style="width:860px; height:273px;">
             <img src="images/parallax_sketch/0_sun.png" alt="" style="position:absolute; left:300px; top:-12px;"/>
+            <div class="fotorama"
+                 data-width="100%"
+                 data-ratio="800/600"
+                 data-minwidth="400"
+                 data-maxwidth="1000"
+                 data-minheight="300"
+                 data-maxheight="100%">
+                <img src="/content/images/gallery_1_1440x900.jpg">
+                <img src="/content/images/gallery_2_1440x900.jpg">
+                <img src="/content/images/gallery_3_1440x900.jpg">
+                <img src="/content/images/gallery_4_1440x900.jpg">
+                <img src="/content/images/gallery_5_1440x900.jpg">
+            </div>
         </div>
         <div class="parallax-layer" style="width:920px; height:274px;">
             <div class="button-header-wrapper">
                 <div class="button-table-bottom">
                     <div class="button-table-bottom-cell">
                         <h1><span class="first-line" data-replace="Date"
-                                  data-base="ƒÓ·Ó ÔÓÊ‡ÎÓ‚‡Ú¸">ƒÓ·Ó ÔÓÊ‡ÎÓ‚‡Ú¸</span></h1>
+                                  data-base="–î–æ–±—Ä–æ –ø–æ–∂–∞–ª–æ–≤–∞—Ç—å">–î–æ–±—Ä–æ –ø–æ–∂–∞–ª–æ–≤–∞—Ç—å</span></h1>
                     </div>
                 </div>
             </div>
@@ -25,7 +38,7 @@
             <div class="button-wrapper">
 					<span class="second-border button-content button-frame" style="position: relative; ">
 						<span class="button-frame-inner" style="transform: translateX(0px) translateY(0px);">
-							<span class="second-line">Õ¿ÿ≈ Ã≈Õﬁ</span>
+							<span class="second-line">–ù–ê–®–ï –ú–ï–ù–Æ</span>
 							<span class="button-frame-hover" style="position: absolute; left: -2px; top: -2px; right: -2px; bottom: -2px; display: block; border: 2px solid rgb(255, 255, 255); transform: translateX(0px) translateY(0px); opacity: 0;"></span>
 						</span>
 					</span>
@@ -34,7 +47,7 @@
         <div class="parallax-layer" style="width:1360px; height:320px;">
             <div class="button-wrapper">
                 <div class="button-content button-arrow" style="opacity: 1;">
-                    <span class="second-line">Õ¿ÿ≈ Ã≈Õﬁ</span>
+                    <span class="second-line">–ù–ê–®–ï –ú–ï–ù–Æ</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="second-border" xmlns:xlink="http://www.w3.org/1999/xlink" width="100px" height="100%" viewBox="0 0 100 50" preserveAspectRatio="xMidYMid meet" zoomAndPan="disable"><line id="e4_line" x1="0" y1="25" x2="98" y2="25" stroke="#fff" style="stroke-width: 2px; vector-effect: non-scaling-stroke; fill: none;"></line><polygon points="100,25 85,34 85,16" style="stroke-width: 2px; vector-effect: non-scaling-stroke; fill: #fff;"></polygon></svg>
                 </div>
             </div>
@@ -42,7 +55,7 @@
         <div class="parallax-layer" style="width:1360px; height:320px;">
             <div class="button-wrapper">
 					<span class="button-content button-text">
-												<a href="/menu" class="second-line" data-replace="14 | 02 | 2017" data-base="Õ¿ÿ≈ Ã≈Õﬁ">Õ¿ÿ≈ Ã≈Õﬁ</a>
+												<a href="/menu" class="second-line" data-replace="14 | 02 | 2017" data-base="–ù–ê–®–ï –ú–ï–ù–Æ">–ù–ê–®–ï –ú–ï–ù–Æ</a>
 						<div id="reservation-trigger"></div>
 					</span>
             </div>
