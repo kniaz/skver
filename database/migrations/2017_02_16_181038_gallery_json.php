fb80482475b701aa96451ef56cadc04a5b83772a
Олександр Галицький <kniaz@nanocoding.com>
2017-02-16 20:18:36 +0200

    json

---
+++ database/migrations/2017_02_16_181038_gallery_json.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Test extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('slides', function ($table) {
+            $table->dropColumn('gallery');
+        });
+        Schema::table('slides', function ($table) {
+            $table->json('gallery')->after('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
