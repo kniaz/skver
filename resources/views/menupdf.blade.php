8a16ea37be0958415283e47fead75e24cb7d0398
jofrey <jofreyswb@gmail.com>
2017-02-20 18:29:46 +0200

    fix save pdf

--- resources/views/menupdf.blade.php
+++ resources/views/menupdf.blade.php
@@ -7,9 +7,9 @@
     <div id="fullpage" class="gallery-page">
 
         <div class="section fp-section section-pdf">
-            <div class="col-md-12 link-pdf">
-                <a href="{{$page['src']}}" target="_blank">Скачать меню</a>
-            </div>
+            {{--<div class="col-md-12 link-pdf">--}}
+                {{--<a href="{{$page['src']}}" target="_blank">Скачать меню</a>--}}
+            {{--</div>--}}
             <div class="col-md-12 menu-pdf">
                 <iframe class="" src="{{$page['src']}}" width="100%"> Ваш браузер не поддерживает плавающие фреймы!</iframe>
             </div>
